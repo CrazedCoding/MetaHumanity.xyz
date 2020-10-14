b551e0138a5f477eb88e2c31c9ab57e82f806619
CrazedCoding <CrazedCoding@gmail.com>
2020-10-14 17:40:24 -0400

    Meow

--- template_engine.py
+++ template_engine.py
@@ -24,6 +24,14 @@ def render_template(www_root, www_path, algorithms_root, short_path, request_hea
                 last_end = occurance.end()+1
             new_body += body[last_end:len(body)]
             body = new_body.encode()
+        elif short_path.lower() == "canvas.html":
+            body = body.decode("utf-8")
+            delimeter = "//ALGORITHM_INSERTION_POINT"
+            index = body.find(delimeter)
+            file_contents = open(aux_path, 'rb').read().decode("utf-8") 
+            new_body += body[0: index]+ file_contents +body[index+len(delimeter): len(body)]
+            body = new_body.encode()
+            
         response_headers.append(("Content-type", ctype))
         response_headers.append(('Content-Length', str(len(body))))
         # response_headers.append(('Access-Control-Allow-Origin', '*'))
