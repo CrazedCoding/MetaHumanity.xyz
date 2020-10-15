cf7197aa77b066532be66d5b19feabbfd76dd5a0
CrazedCoding <CrazedCoding@gmail.com>
2020-10-15 02:26:37 -0400

    Meow

--- template_engine.py
+++ template_engine.py
@@ -16,15 +16,16 @@ def get_param_value(query_params, param):
 
 
 def render_template(server_root, query_params, www_root, www_path, algorithms_root, short_path, request_headers, response_headers, ctype, parsed):
-    body = open(www_path, 'rb').read()
+    body = ""
     if short_path.lower().startswith("/algorithms/"):
         algorithm_file = os.path.join(server_root, short_path.lower())
         if os.path.commonpath((algorithms_root, algorithm_file)) and os.path.exists(algorithm_file):
-            pass
+            body = open(algorithm_file, 'rb').read()
         else:
             print("404 ALGORITHM NOT FOUND")
             return HTTPStatus.NOT_FOUND, [], b'404 ALGORITHM NOT FOUND'
     if short_path.lower() == "index.html":
+        body = open(www_path, 'rb').read()
         body = body.decode("utf-8")
         pattern = r'\{\{.*?\}\}'
         last_end = 0
