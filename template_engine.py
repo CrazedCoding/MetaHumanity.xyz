052830c3d3b98b08cc481780ba3ee7262ec77361
CrazedCoding <CrazedCoding@gmail.com>
2020-10-21 23:20:53 -0400

    Meow

--- template_engine.py
+++ template_engine.py
@@ -13,14 +13,10 @@ from datetime import datetime
 
 browse_template = """
 
-<h1 style="margin-bottom:5%; padding-top: 14px; padding-bottom: 14px; text-align:center; color:#fff; font-size: 14px !important; width:max-content; background-color: rgba(0,0,0,.75); border-radius: 0px; border: 1px solid #fff !important;">
-
-    <a>Search Criteria:</a>
+<h1 style="margin-bottom:5%; padding-top: 14px; padding-bottom: 14px; text-align:center; color:#fff; font-size: 14px !important; width:100%;">
     <input id="search_criteria"
-        style="width:inherit; text-align:center !important; color:#fff; text-align: left;"
+        style="width:inherit; text-align:center !important; color:#000; background-color:#fff; text-align: left; width:50%"
         placeholder="Enter search criteria..." value="">
-    <br>
-    <br>
     <button type="button" class="btn search-btn btn-outline-success mx-auto"
         onclick="state.sortViews()">Views</button>
     <button type="button" class="btn search-btn btn-outline-success mx-auto"
@@ -41,7 +37,9 @@ browse_template = """
         onclick="state.nextPage()">&gt;</button>
     <br>
     <br>
+    <div style=" width:max-content; background-color: rgba(0,0,0,.75); border-radius: 0px; border: 1px solid #fff !important;">
     Page {{current_page}} of {{max_page}}
+    </div>
 </h1>
 <br>
 """
